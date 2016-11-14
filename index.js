d0747b24d3bddaa4891fcfd99917cd34b3970cf2
Paul Anderson <paul.anderson@uberops.com>
2016-11-14 14:41:30 -0500

    adding the http api handler in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const http = (apiMethod) => {
+   return (req, res, next) => apiMethod(req)
+   .then((apiResponse) => {
+      res.json(apiResponse || {})
+   }).catch(next)
+}
+
+export {
+   http
+}
