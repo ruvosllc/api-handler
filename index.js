44e323f4d147dfdc8fa7c459db8e6b2e2c76b42b
Paul Anderson <paul.anderson@uberops.com>
2016-12-19 13:00:30 -0500

    adding direct status promise functions. called like apiHandler[404]()

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const http = require('./lib/http')
 const logger = require('./lib/logger')
 const error = require('./lib/error')
 const reject = require('./lib/reject')
+const statuses = require('./lib/statuses')
 
 const apiHandler = {
   http,
   logger,
   error,
+  reject,
 }
+
+Object.assign(apiHandler, statuses)
 
 module.exports = apiHandler
