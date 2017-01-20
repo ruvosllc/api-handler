0729e2a5d2af98925af0003ab8f5588dbce78113
Paul Anderson <paul.anderson@uberops.com>
2017-01-20 13:57:12 -0500

    export validate

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const logger = require('./lib/logger')
 const error = require('./lib/error')
 const reject = require('./lib/reject')
 const statuses = require('./lib/statuses')
+const validate = require('./lib/validate')
 
 const apiHandler = {
   http,
   logger,
   error,
   reject,
+  validate,
 }
 
 Object.assign(apiHandler, statuses)
