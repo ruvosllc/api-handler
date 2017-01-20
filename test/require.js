2800f0dc06c29309f7b0f7b93751bf15034cd4b3
Paul Anderson <paul.anderson@uberops.com>
2017-01-20 17:43:07 -0500

    add error message handling & generation to require

--- test/require.js
+++ test/require.js
@@ -7,7 +7,7 @@ require('should')
 const describe = mocha.describe
 const it = mocha.it
 
-describe('require validates things and throws 400s', () => {
+describe('require validates things by throwing 400s', () => {
   it('when nothing is provided', () => {
     requ.should.throw({ status: 400 })
     requ.bind(null, true).should.not.throw()
@@ -29,5 +29,13 @@ describe('require validates things and throws 400s', () => {
   it('when the actual value does not pass the provided validator', () => {
     requ.bind(null, 'thing', () => false).should.throw({ status: 400 })
     requ.bind(null, 'thing', () => true).should.not.throw()
+  })
+  it('and will handle accept messages', () => {
+    requ.bind(null, false, true, 'a message').should.throw({ status: 400, message: 'a message' })
+    requ.bind(null, { a: 'b' }, { a: 'c' }, 'a message').should.throw({ status: 400, message: 'a message' })
+  })
+  it('and will generate error messages for object validations', () => {
+    requ.bind(null, { body: { thing: 'thing' } }, { body: { thing: 'notThing' } })
+    .should.throw({ status: 400, message: 'body.thing is invalid' })
   })
 })
